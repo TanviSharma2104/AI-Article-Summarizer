782974fa373a64842cb241da9a458328f37e079c
TanviSharma2104 <tanvisharma2703@gmail.com>
2024-01-10 14:40:41 +0530

    Home page description addded

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import {logo} from '../assets';
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-        <nav className='flex justify-between items-center'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 
             <button 
@@ -13,6 +13,13 @@ const Hero = () => {
                 onClick={()=> window.open('https://github.com/TanviSharma2104/AI-Article-Summarizer')}>
                 GitHub</button>
         </nav>
+
+        <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden' />
+          <span className='orange_gradient'> OpenAI GPT-4</span></h1>
+          <h2 className='desc'>
+            Simplify your reading with Summize, an open-source article summarizer that
+            transforms length articles into clear and concise summaries
+          </h2>
     </header>
   )
 }
